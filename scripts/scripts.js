6b75dfc2c6d815862a6a61778dc159f6d22ea00b
miltonhuynh <miltonhuynh@gmail.com>
2022-03-23 22:03:01 -0700

    Added answers

---
+++ scripts/scripts.js
@@ -0,0 +1,36 @@
+const button = document.querySelector('button');
+const ball = document.getElementById('ball');
+
+function random() {
+    return Math.floor(Math.random() * 19);
+}
+
+function ask(){
+    var images = [
+        'images\\magic8ball_1.png',
+        'images\\magic8ball_2.png',
+        'images\\magic8ball_3.png',
+        'images\\magic8ball_4.png',
+        'images\\magic8ball_5.png',
+        'images\\magic8ball_6.png',
+        'images\\magic8ball_7.png',
+        'images\\magic8ball_8.png',
+        'images\\magic8ball_9.png',
+        'images\\magic8ball_10.png',
+        'images\\magic8ball_11.png',
+        'images\\magic8ball_12.png',
+        'images\\magic8ball_13.png',
+        'images\\magic8ball_14.png',
+        'images\\magic8ball_15.png',
+        'images\\magic8ball_16.png',
+        'images\\magic8ball_17.png',
+        'images\\magic8ball_18.png',
+        'images\\magic8ball_19.png',
+        'images\\magic8ball_20.png',
+    ]
+
+    ball.src = images[random()];
+}
+
+button.addEventListener("click",ask);
+
