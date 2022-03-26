c965ae420d9ad6503dfb9e1816d79dfdf0e817e7
miltonhuynh <miltonhuynh@gmail.com>
2022-03-25 21:30:02 -0700

    Accounted for empty input

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,54 +28,59 @@ function shrink() {
   }
 }
 
-function ask() {     
-    ball.style.transform = "translate(100px)";
-    setTimeout(function() {
-        ball.style.transform = "translate(-100px)";
-    }, 500);
-    setTimeout(function() {
+function ask() {
+    if (document.getElementById('ask').innerHTML == "") {
+        alert("You didn't type in a question!");
+    } else {
+    
         ball.style.transform = "translate(100px)";
-    }, 1000);
-    setTimeout(function() {
-        ball.style.transform = "translate(0px)";
-    }, 1500);
+        setTimeout(function() {
+            ball.style.transform = "translate(-100px)";
+        }, 500);
+        setTimeout(function() {
+            ball.style.transform = "translate(100px)";
+        }, 1000);
+        setTimeout(function() {
+            ball.style.transform = "translate(0px)";
+        }, 1500);
 
 
-    var images = [
-        'images\\magic8ball_1.png',
-        'images\\magic8ball_2.png',
-        'images\\magic8ball_3.png',
-        'images\\magic8ball_4.png',
-        'images\\magic8ball_5.png',
-        'images\\magic8ball_6.png',
-        'images\\magic8ball_7.png',
-        'images\\magic8ball_8.png',
-        'images\\magic8ball_9.png',
-        'images\\magic8ball_10.png',
-        'images\\magic8ball_11.png',
-        'images\\magic8ball_12.png',
-        'images\\magic8ball_13.png',
-        'images\\magic8ball_14.png',
-        'images\\magic8ball_15.png',
-        'images\\magic8ball_16.png',
-        'images\\magic8ball_17.png',
-        'images\\magic8ball_18.png',
-        'images\\magic8ball_19.png',
-        'images\\magic8ball_20.png',
-    ]
+        var images = [
+            'images\\magic8ball_1.png',
+            'images\\magic8ball_2.png',
+            'images\\magic8ball_3.png',
+            'images\\magic8ball_4.png',
+            'images\\magic8ball_5.png',
+            'images\\magic8ball_6.png',
+            'images\\magic8ball_7.png',
+            'images\\magic8ball_8.png',
+            'images\\magic8ball_9.png',
+            'images\\magic8ball_10.png',
+            'images\\magic8ball_11.png',
+            'images\\magic8ball_12.png',
+            'images\\magic8ball_13.png',
+            'images\\magic8ball_14.png',
+            'images\\magic8ball_15.png',
+            'images\\magic8ball_16.png',
+            'images\\magic8ball_17.png',
+            'images\\magic8ball_18.png',
+            'images\\magic8ball_19.png',
+            'images\\magic8ball_20.png',
+        ]
 
-    setTimeout(function() {
-        ball.src = images[random()];
-    }, 2500);
-    setTimeout(function() {
-        grow();
-    }, 3000);
-    setTimeout(function() {
-        shrink();
-    },7000);
-    setTimeout(function () {
-        ball.src = 'images\\magic8ball_start.png';
-    }, 8000);
+        setTimeout(function() {
+            ball.src = images[random()];
+        }, 2500);
+        setTimeout(function() {
+            grow();
+        }, 3000);
+        setTimeout(function() {
+            shrink();
+        },7000);
+        setTimeout(function () {
+            ball.src = 'images\\magic8ball_start.png';
+        }, 8000);
+    }
 }
 
 button.addEventListener("click",ask);
